535b4d22ca47853f340812b50faf317412dce9de
brialxs <110439935+brialxs@users.noreply.github.com>
2022-09-13 02:05:36 -0300

    entrega3_2

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,7 @@
 const INFO_PRODUCT =  PRODUCT_INFO_URL + localStorage.getItem("productID") + EXT_TYPE;
 const containerInfo = document.getElementById("infoContainer");
+const comments = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("productID") + EXT_TYPE;
+const comentarios = document.getElementById("comentarios");
 
 console.log(INFO_PRODUCT);
 
@@ -19,9 +21,40 @@ document.addEventListener("DOMContentLoaded", function(p) {
             infoProducts = resultObj.data
             showProductInfo(infoProducts)
             //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
-        }
+        }   
     });
+
+    getJSONData(comments).then(function(resultObj){
+      if (resultObj.status === "ok"){
+          infoComments = resultObj.data
+          showCategoriesList(infoComments)
+          //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+      }   
+  });
 });
+
+function addStar() {
+  document.showCategoriesList.score.innerHTML = ` <span class="fa fa-star"></span> `
+}
+
+function showCategoriesList(){
+
+  let htmlContentToAppend = "";
+  for(let i = 0; i < infoComments.length; i++){
+      let comentario = infoComments[i];
+          htmlContentToAppend += `
+          <div onclick="comentarioID(${comentario.product})" class="list-group-item list-group-item-action cursor-active"  >
+            <div class="col-3">
+              <label id="score"><strong>${comentario.user}</strong> - ${comentario.dateTime} - </label>
+              <p>${comentario.description} </p>
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="stretched-link"></a>
+            </div> 
+          </div>
+          `
+      }
+
+      comentarios.innerHTML = htmlContentToAppend;
+  };
 
 function showProductInfo(){
 
@@ -47,26 +80,42 @@ function showProductInfo(){
                     </div>
                 </div>    
 
-                <div class="container">
+                <div id="carouselExampleIndicators" class="carousel slide w-50" data-bs-ride="carousel">
+                  <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
+                  </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="${info.images[0]}" class="d-block w-100" alt="0">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="${info.images[1]}" class="d-block w-100" alt="1">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="${info.images[2]}" class="d-block w-100" alt="2">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="${info.images[3]}" class="d-block w-100" alt="3">
+                    </div>
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                <br>
+                <br>
+                <br>                  
+                </div>
 
-                <div class="row">
-               
-                  <div class="col"> 
-                    <img class="img-fluid border border-5" max-width="100%" height="auto" src="${info.images[0]}">
-                  </div>
-              
-                  <div class="col">
-                    <img class="img-fluid border border-5" max-width="100%" height="auto" src="${info.images[1]}">
-                  </div>
+                <label>Comentarios</label>
 
-                  <div class="col">
-                    <img class="img-fluid border border-5" max-width="100%" height="auto" src="${info.images[2]}">
-                  </div>
-
-                  <div class="col">
-                    <img class="img-fluid border border-5" max-width="100%" height="auto" src="${info.images[3]}">
-                  </div>
-               
                 </div>
               </div>
             `
