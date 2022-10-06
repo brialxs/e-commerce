436743876eb03ed0d624455b8f47536bb7c86f8b
brialxs <110439935+brialxs@users.noreply.github.com>
2022-10-06 11:34:31 -0300

    entrega5_02

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,39 @@
-const cartBuy = CART_INFO_URL + 25801 + EXT_TYPE;
+const CART_BUY = CART_INFO_URL + "25801" + EXT_TYPE;
 const bodyTable = document.getElementById("bodyTable");
 
 document.addEventListener("DOMContentLoaded", function(p) {
     
-    getJSONData(cartBuy).then(function(resultObj){
+    getJSONData(CART_BUY).then(function(resultObj){
         if (resultObj.status === "ok"){
-            infoProducts = resultObj.data
-            showCart(cartBuy)
+            cart = resultObj.data
+            showCart()
         }   
     });
 });
 
+
+
 function showCart (){
 
+    let { id, name, count, unitCost, currency, image } = CART_BUY;
+
     let addCart = "";
-    for (let cart of cartBuy) {
+    for (let cart of CART_BUY) {
+        
     addCart += `
-    <tbody">
+    <tbody>
         <tr>
-        <th scope="row">1</th>
-        <td>${cart.name}</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-    </tr>
-  
+            <th scope="row">${cart.id}</th>
+            <td>${cart.name}</td>
+            <td>0</td>
+            <td>@mdo</td>
+         </tr>
      </tbody>
     `
     }
 
     bodyTable.innerHTML = addCart;
-    console.log(cartBuy)
+    console.log(CART_BUY)
 
 };
+
